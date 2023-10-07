8066cb7ad72f493207b93a15a58a1a885cd976e2
Tony Bernardo de Lima <dev-tony@Tonys-MacBook-Pro.local>
2023-10-06 20:47:42 -0500

    Fix Icon button

--- apps/web/components/examples/index.tsx
+++ apps/web/components/examples/index.tsx
@@ -1,7 +1,7 @@
 import { ButtonExample } from '@/components/examples/buttons/button-example'
 import { DestructiveExample } from '@/components/examples/buttons/destructive-example'
 import { GhostExample } from '@/components/examples/buttons/ghost-example'
-import { IconButtonExample } from '@/components/examples/buttons/Icon-button-example'
+import { IconButtonExample } from '@/components/examples/buttons/Icon-example'
 import { OutlineExample } from '@/components/examples/buttons/outline-example'
 import { SecondaryExample } from '@/components/examples/buttons/secondary-example'
 import { InputExample } from '@/components/examples/inputs/input-example'
